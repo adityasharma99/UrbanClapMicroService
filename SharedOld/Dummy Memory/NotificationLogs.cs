8748b60dd6a9c9ce7bb3e6c57beeac26693cfc98
Aditya Sharma <aditya.sharma02@nagarro.com>
2022-04-18 13:06:29 +0530

    latest

---
+++ SharedOld/Dummy Memory/NotificationLogs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared
+{
+    ///Notification Class 
+    public class NotificationLogs
+    {
+        public string ServiceName { get; set; }
+        public string Message { get; set; }
+    }
+}
